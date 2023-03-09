856d18d4ad0c44687dcce1c7c2ba4e57e89785e7
Joujou <jawherjabri2000@gmail.com>
2023-03-09 17:06:46 +0100

    filter test

--- dashboard.py
+++ dashboard.py
@@ -52,85 +52,111 @@ import pandas as pd
 import panel as pn
 import hvplot.pandas
 import plotly.graph_objs as go
+import param
 
 # Load the dataset
 df = pd.read_csv('StudentsPerformance.csv')
 
-# Define the filters dictionary and filter_table widget
-filters = {
-    'gender': {'type': 'input', 'func': 'like', 'placeholder': 'Enter gender'},
-    'race/ethnicity': {'type': 'input','placeholder': 'Enter race'},
-    'parental level of education': {'type': 'input', 'func': 'like', 'placeholder': 'Enter parental level of education'},
-    'lunch': {'type': 'input', 'func': 'like', 'placeholder': 'Enter director'},
-    'test preparation course': {'type': 'input', 'func': 'like', 'placeholder': 'Enter writer'},
-    'reading score': {'type': 'number', 'func': '>=', 'placeholder': 'Enter minimum reading score'},
-    'math score': {'type': 'number', 'func': '>=', 'placeholder': 'Enter minimum math score'},
-    'writing score': {'type': 'number', 'func': '>=', 'placeholder': 'Enter minimum rating'}
-}
-filter_table = pn.widgets.Tabulator(df, pagination='remote', layout='fit_columns', page_size=10, sizing_mode='stretch_width', header_filters=filters)
 
-gender = pn.widgets.Select(name="gender", options=(list)(df["gender"].unique()))
-race_ethnicity = pn.widgets.Select(name="race/ethnicity", options=(list)(df["race/ethnicity"].unique()))
-parental_level_of_education = pn.widgets.Select(name="parental level of education", options=(list)(df["parental level of education"].unique()))
-lunch = pn.widgets.Select(name="Lunch", options=(list)(df["lunch"].unique()))
-test_preparation_course = pn.widgets.Select(name="test preparation course", options=(list)(df["test preparation course"].unique()))
-math = pn.widgets.FloatSlider(name="Math score", start=0, end=100, value=50)
-reading = pn.widgets.FloatSlider(name="Reading score", start=0, end=100, value=50)
-writing = pn.widgets.FloatSlider(name="Writing score", start=0, end=100, value=50)
+class student_EDA (param.Parameterized): 
 
 
+    gender_list= (list)(df["gender"].unique())
+    gender_list.append('ALL')
+    gender_widget  =param.ObjectSelector(default='male',objects=gender_list)
 
-# Define the scatter plot and histogram
-scatter_plot = df.hvplot.scatter(x='math score', y='reading score', c='writing score', cmap='viridis')
-histogram = df.hvplot.hist('writing score', bins=20, width=500)
+    race_ethnicity_list= (list)(df["race/ethnicity"].unique())
+    race_ethnicity_list.append('ALL')
+    race_ethnicity_widget  = param.ObjectSelector(default='ALL',objects=race_ethnicity_list)
 
-# Bar chart
-bar_chart_data = df.groupby('gender').mean()[['math score', 'reading score', 'writing score']]
-bar_chart = go.Figure(
-    data=[
-        go.Bar(name='Math score', x=bar_chart_data.index, y=bar_chart_data['math score']),
-        go.Bar(name='Reading score', x=bar_chart_data.index, y=bar_chart_data['reading score']),
-        go.Bar(name='Writing score', x=bar_chart_data.index, y=bar_chart_data['writing score']),
-    ],
-    layout=go.Layout(title='Average Scores by Gender')
-)
-bar_chart_panel = pn.pane.Plotly(bar_chart, sizing_mode='stretch_both')
+    parental_level_of_education_list = (list)(df["parental level of education"].unique())
+    parental_level_of_education_list='ALL'
+    parental_level_of_education_widget  = param.ObjectSelector(default='ALL',objects=parental_level_of_education_list)
+    
+    lunch_list= (list)(df["lunch"].unique())
+    lunch_list.append('ALL')
+    lunch = param.ObjectSelector(default='ALL',objects=lunch_list)
 
-# Pie chart
-pie_chart_data = df['race/ethnicity'].value_counts()
-pie_chart = go.Figure(
-    data=[go.Pie(labels=pie_chart_data.index, values=pie_chart_data.values)],
-    layout=go.Layout(title='Distribution of Students by Race/Ethnicity')
-)
-pie_chart_panel = pn.pane.Plotly(pie_chart, sizing_mode='stretch_both')
+    test_prep_list= (list)(df["test preparation course"].unique())
+    test_prep_list.append('ALL')
+    test_preparation_course_widget  = param.ObjectSelector(default='ALL',objects=test_prep_list)
+    
+    math_widget = param.Number(50, bounds=(0, 100))
+    reading_widget  = param.Number(50, bounds=(0, 100))
+    writing_widget  = param.Number(50, bounds=(0, 100))
 
-# Combine the filter_table and sidebar into a grid layout
-grid = pn.Row(
-    pn.Spacer(width=10),  # Add some spacing between the sidebar and the filter_table
-    pn.Column(filter_table, height=500, sizing_mode='stretch_both', width_policy='max'),
-    sizing_mode='stretch_both'
-)
-# Combine the histogram and scatter_plot into a grid layout
-grid1 = pn.Row(
-    pn.Column(histogram, height=500, sizing_mode='stretch_both', width_policy='max'),
-    pn.Spacer(width=20),  # Add some spacing between the sidebar and the filter_table
-    pn.Column(scatter_plot, height=500, sizing_mode='stretch_both', width_policy='max'),
-    sizing_mode='stretch_both'
-)
+    #@pn.depends('gender_widget')
+    def table (self):
+         # Define the filters dictionary and filter_table widget
+        filters = {
+        'gender': {'type': 'input', 'func': 'like', 'placeholder': 'Enter gender'},
+        'race/ethnicity': {'type': 'input','placeholder': 'Enter race'},
+        'parental level of education': {'type': 'input', 'func': 'like', 'placeholder': 'Enter parental level of education'},
+        'lunch': {'type': 'input', 'func': 'like', 'placeholder': 'Enter director'},
+        'test preparation course': {'type': 'input', 'func': 'like', 'placeholder': 'Enter writer'},
+        'reading score': {'type': 'number', 'func': '>=', 'placeholder': 'Enter minimum reading score'},
+        'math score': {'type': 'number', 'func': '>=', 'placeholder': 'Enter minimum math score'},
+        'writing score': {'type': 'number', 'func': '>=', 'placeholder': 'Enter minimum rating'}
+         }
+    
+        filter_table =pn.widgets.Tabulator(df, pagination='remote', layout='fit_columns', page_size=10, sizing_mode='stretch_width', header_filters=filters)
+        return  pn.Row(
+            pn.Spacer(width=10),  # Add some spacing between the sidebar and the filter_table
+            pn.Column(filter_table, height=500, sizing_mode='stretch_both', width_policy='max'),
+            sizing_mode='stretch_both'
+        )  
+    @param.depends('math_widget',watch=True, on_init=False)
+    def plots(self):
+        
+        if (self.gender_widget!='ALL'):
+            df1 = df[df['gender']==self.gender_widget]
+        # Define the scatter plot and histogram
+        scatter_plot = df1.hvplot.scatter(x='math score', y='reading score', c='writing score', cmap='viridis')
+        histogram = df1.hvplot.hist('writing score', bins=20, width=500)
+        # Bar chart
+        bar_chart_data = df1.groupby('gender').mean()[['math score', 'reading score', 'writing score']]
+        bar_chart = go.Figure(
+            data=[
+                go.Bar(name='Math score', x=bar_chart_data.index, y=bar_chart_data['math score']),
+                go.Bar(name='Reading score', x=bar_chart_data.index, y=bar_chart_data['reading score']),
+                go.Bar(name='Writing score', x=bar_chart_data.index, y=bar_chart_data['writing score']),
+            ],
+            layout=go.Layout(title='Average Scores by Gender')
+        )
+        bar_chart_panel = pn.pane.Plotly(bar_chart, sizing_mode='stretch_both')
 
-# Combine the new plots into a grid layout with the existing ones
-grid2 = pn.Row(
-    pn.Column(bar_chart_panel, height=500, sizing_mode='stretch_both', width_policy='max'),
-    pn.Spacer(width=20),
-    pn.Column(pie_chart_panel, height=500, sizing_mode='stretch_both', width_policy='max'),
-    sizing_mode='stretch_both'
-)
+        # Pie chart
+        pie_chart_data = df['race/ethnicity'].value_counts()
+        pie_chart = go.Figure(
+            data=[go.Pie(labels=pie_chart_data.index, values=pie_chart_data.values)],
+            layout=go.Layout(title='Distribution of Students by Race/Ethnicity')
+        )
+        pie_chart_panel = pn.pane.Plotly(pie_chart, sizing_mode='stretch_both')
+
+        # Combine the histogram and scatter_plot into a grid layout
+        grid1 = pn.Row(
+            pn.Column(histogram, height=500, sizing_mode='stretch_both', width_policy='max'),
+            pn.Spacer(width=20),  # Add some spacing between the sidebar and the filter_table
+            pn.Column(scatter_plot, height=500, sizing_mode='stretch_both', width_policy='max'),
+            sizing_mode='stretch_both'
+        )
+
+        # Combine the new plots into a grid layout with the existing ones
+        grid2 = pn.Row(
+            pn.Column(bar_chart_panel, height=500, sizing_mode='stretch_both', width_policy='max'),
+            pn.Spacer(width=20),
+            pn.Column(pie_chart_panel, height=500, sizing_mode='stretch_both', width_policy='max'),
+            sizing_mode='stretch_both'
+        )
+        return grid1 
+    
+dashboard = student_EDA()
 
 # Define a custom panel template with the grid layout
 template = pn.template.FastListTemplate(
     title='Filter Table Template',
-    main=[grid,grid1,grid2],
-     sidebar=[pn.pane.Markdown("## Filters"),math,reading,writing,gender, race_ethnicity, parental_level_of_education, lunch, test_preparation_course]
+    main=[dashboard.table,dashboard.plots],
+     sidebar=[pn.Param(dashboard.param,widgets={'math_widget':pn.widgets.FloatSlider,'reading_widget':pn.widgets.FloatSlider,'writing_widget':pn.widgets.FloatSlider, 'race_ethnicity_widget': pn.widgets.Select,'gender_widget': pn.widgets.Select, 'parental_level_of_education_widget': pn.widgets.Select, 'lunch_widget': pn.widgets.Select, 'test_preparation_course_widget': pn.widgets.Select})]
 )
 
 # Show the template in a browser tab
